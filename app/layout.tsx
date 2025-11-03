772ee77850742b571c27350e727405a35f04ede2
moathssnck <myneww00@gmail.com>
2025-11-03 21:42:56 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
         <head>
           <script
             async
-            src="https://www.googletagmanager.com/gtag/js?id=AW-17685063189"
+            src="https://www.googletagmanager.com/gtag/js?id=AW-XXX"
           />
           <script
             dangerouslySetInnerHTML={{
@@ -30,7 +30,7 @@ export default function RootLayout({
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', 'AW-17685063189');
+              gtag('config', 'AW-XXX');
             `,
             }}
           />
